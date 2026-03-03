3a8f5aee282418205226f78e46b12a8fdedde68d
BHOGATHI KALPANA <123709152+kalpanareddy-29@users.noreply.github.com>
2026-03-03 10:01:11 +0530

    Update largest_smallest_element.py

--- Arrays/largest_smallest_element.py
+++ Arrays/largest_smallest_element.py
@@ -1,1 +1,16 @@
+"""
+Program: Find Largest and Smallest Element in a List.
+Description: This program finds the maximum and minimum element in a given list.
+"""
 
+#num=map(int,input().split())
+num=[3,4,5,8,32]
+max=min=num[0]
+for i in num:
+    if(max<i):
+        max=i
+    if(min>i):
+        print("hii")
+        min=i
+print(f"{max} is maximum and {min} is minimum")
+    
